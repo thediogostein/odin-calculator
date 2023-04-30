8e3c26f05293535f213ebd9c50a58bafa5a5b4ed
bruxo <diogo.stein@outlook.com.br>
2023-04-30 17:31:38 -0300

    Add functionality to the calculator

--- js/script.js
+++ js/script.js
@@ -1,93 +1,96 @@
-// -- GLOBAL VARIABLES
+let a = "";
+let b = "";
+let operator = "";
 
-// -- Elements
-const outPrevious = document.querySelector("#out-previous");
-const outCurrent = document.querySelector("#out-current");
-const btnAc = document.querySelector("#btn-ac");
-const btnBackspace = document.querySelector("#btn-backspace");
-const btnAdd = document.querySelector("#btn-add");
-const btnSub = document.querySelector("#btn-subtract");
-const btnMul = document.querySelector("#btn-multiply");
-const btnDiv = document.querySelector("#btn-divide");
+const btnNumbers = document.querySelectorAll(".btn-number");
+const btnOperators = document.querySelectorAll(".btn-operator");
 const btnEquals = document.querySelector("#btn-equals");
+const btnAllClear = document.querySelector("#btn-all-clear");
+let outResult = document.querySelector("#out-result");
 
-const btn = document.querySelectorAll(".btn-number");
-
-// -- BUTTONS -- event listeners
-
-// -- 0-9
-btn.forEach((item) => {
-  item.addEventListener("click", (event) => {
-    // outCurrent.innerText += +event.target.innerText;
-    let num = event.target.innerText;
-    outCurrent.innerText += num;
+btnNumbers.forEach((btnNumber) => {
+  btnNumber.addEventListener("click", () => {
+    if (operator === "") {
+      a += btnNumber.innerText;
+      updateDisplay();
+    } else {
+      b += btnNumber.innerText;
+      updateDisplay();
+    }
   });
 });
 
-// -- AC
-btnAc.addEventListener("click", () => {
-  outCurrent.innerText = "";
+btnOperators.forEach((btnOperator) => {
+  btnOperator.addEventListener("click", () => {
+    if (a !== "") {
+      operator = btnOperator.value;
+      updateDisplay();
+    }
+  });
 });
 
-// -- Backspace
-btnBackspace.addEventListener("click", () => {
-  console.log("backspace");
-});
-
-// -- Add
-btnAdd.addEventListener("click", () => {
-  console.log("add");
-});
-
-// -- Subtract
-btnSub.addEventListener("click", () => {
-  console.log("subtract");
-});
-
-// -- Multiply
-btnMul.addEventListener("click", () => {
-  console.log("multiply");
-});
-
-// -- Divide
-btnDiv.addEventListener("click", () => {
-  console.log("divide");
-});
-
-// -- Equals
 btnEquals.addEventListener("click", () => {
-  console.log("equals");
+  if (a !== "" && b !== "" && operator !== "") {
+    const total = calculate(a, b, operator);
+    outResult.innerText = total;
+    a = total;
+    b = "";
+  }
 });
 
-function add(num1, num2) {
-  console.log(num1 + num2);
+btnAllClear.addEventListener("click", () => {
+  allClear();
+});
+
+// update display
+function updateDisplay() {
+  outResult.innerText = `${a} ${operator} ${b}`;
 }
 
-function subtract(num1, num2) {
-  console.log(num1 - num2);
+// ac
+function allClear() {
+  a = "";
+  b = "";
+  operator = "";
+  outResult.innerText = "";
 }
 
-function multiply(num1, num2) {
-  console.log(num1 * num2);
-}
-
-function divide(num1, num2) {
-  console.log(num1 / num2);
-}
-
-function operate(num1, num2, operator) {
+// calculate
+function calculate(a, b, operator) {
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case "+":
-      add(num1, num2);
+      return add(a, b);
       break;
     case "-":
-      subtract(num1, num2);
+      return subtract(a, b);
       break;
-    case "x":
-      multiply(num1, num2);
+    case "*":
+      return multiply(a, b);
       break;
-    case "%":
-      divide(num1, num2);
+    case "/":
+      return divide(a, b);
       break;
   }
+}
+
+// add
+function add(a, b) {
+  return a + b;
+}
+
+// subtract
+function subtract(a, b) {
+  return a - b;
+}
+
+// multiply
+function multiply(a, b) {
+  return a * b;
+}
+
+// divide
+function divide(a, b) {
+  return a / b;
 }
