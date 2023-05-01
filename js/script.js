4f31f2de16b5d91716178453668caef2b40aa9f9
bruxo <diogo.stein@outlook.com.br>
2023-05-01 11:34:25 -0300

    Add decimal functionality

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@ let a = "";
 let b = "";
 let operator = "";
 let divisionByZero = false;
+let decimalA = false;
+let decimalB = false;
 
 const btnNumbers = document.querySelectorAll(".btn-number");
 const btnOperators = document.querySelectorAll(".btn-operator");
 const btnEquals = document.querySelector("#btn-equals");
 const btnAllClear = document.querySelector("#btn-all-clear");
+const btnDecimal = document.querySelector("#btn-decimal");
 let outResult = document.querySelector("#out-result");
+
+//criar variaveis para o backspace button e o decimal number
+//adicionar funcionalidades
 
 btnNumbers.forEach((btnNumber) => {
   btnNumber.addEventListener("click", () => {
@@ -31,6 +37,7 @@ btnOperators.forEach((btnOperator) => {
         outResult.innerText = total;
         a = total;
         b = "";
+        decimalB = false;
       }
       if (a !== "") {
         operator = btnOperator.value;
@@ -46,11 +53,25 @@ btnEquals.addEventListener("click", () => {
     outResult.innerText = total;
     a = total;
     b = "";
+    decimalB = false;
   }
 });
 
 btnAllClear.addEventListener("click", () => {
   allClear();
+});
+
+btnDecimal.addEventListener("click", () => {
+  if (operator === "" && decimalA === false) {
+    a += btnDecimal.innerText;
+    decimalA = true;
+    updateDisplay();
+  }
+  if ((operator !== "") & (decimalB === false)) {
+    b += btnDecimal.innerText;
+    decimalB = true;
+    updateDisplay();
+  }
 });
 
 // update display
@@ -62,6 +83,8 @@ function updateDisplay() {
 function allClear() {
   a = "";
   b = "";
+  decimalA = false;
+  decimalB = false;
   operator = "";
   outResult.innerText = "";
   divisionByZero = false;
@@ -89,17 +112,17 @@ function calculate(a, b, operator) {
 
 // add
 function add(a, b) {
-  return a + b;
+  return (a + b) % 1 !== 0 ? (a + b).toFixed(2) : a + b;
 }
 
 // subtract
 function subtract(a, b) {
-  return a - b;
+  return (a - b) % 1 !== 0 ? (a - b).toFixed(2) : a - b;
 }
 
 // multiply
 function multiply(a, b) {
-  return a * b;
+  return (a * b) % 1 !== 0 ? (a * b).toFixed(2) : a * b;
 }
 
 // divide
